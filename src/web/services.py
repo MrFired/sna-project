2a7c819fb2f3183050aff6fbc7d1c4a54c96c9d8
MrFired <Milgram_S@mail.ru>
2023-05-19 21:31:04 +0300

    Add web content (#2) (#5)
    * Add files via upload
    
    * Update style.css
    
    * Add files via upload
    
    * Update app.py
    
    * Update services.py
    
    * now it works
    
    * nice page
    
    ---------
    
    Co-authored-by: Hallejujah123 <57189226+Hallejujah123@users.noreply.github.com>
    Co-authored-by: ADari-Ka <ADari.Ka@yandex.ru>

--- src/web/services.py
+++ src/web/services.py
@@ -1,7 +1,8 @@
-from pathlib import Path
-
+from src import ROOT_DIR
+from src.config import Config
 from src.typings import RawHTML, as_raw_html
 
 
-def load_html(path: Path) -> RawHTML:
+def load_html(file_name: str, config: Config) -> RawHTML:
+    path = ROOT_DIR / config.STATIC_FILES / file_name
     return as_raw_html(path.read_text())
