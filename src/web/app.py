93f0bfb2bae5a0a3f4dec835012003e445f8eac9
Hallejujah123 <57189226+Hallejujah123@users.noreply.github.com>
2023-05-19 13:28:56 +0300

    Update app.py

--- src/web/app.py
+++ src/web/app.py
@@ -1,22 +1,25 @@
 from fastapi import FastAPI
 from fastapi.responses import HTMLResponse
+from fastapi.staticfiles import StaticFiles
 
 from src.config import Config
+from src.services import load_html
+
 
 app = FastAPI()
 config = Config()
 
+# Serve static files
+if config.STATIC_FILES:
+    app.mount("/static", StaticFiles(directory=str(config.STATIC_FILES)), name="static")
+
 
 @app.get("/")
 async def main_page():
-    html_content = """
-    <html>
-        <head>
-            <title>HoYoClub</title>
-        </head>
-        <body>
-            Just a text.
-        </body>
-    </html>
-    """
-    return HTMLResponse(content=html_content)
+    index_html = load_html(config.STATIC_FILES / "index.html")
+    return HTMLResponse(content=index_html)
+
+
+if __name__ == "__main__":
+    import uvicorn
+    uvicorn.run(app, host="0.0.0.0", port=config.PORT)
