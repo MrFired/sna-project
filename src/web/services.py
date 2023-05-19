f9fa37702af5d3417b30b7e1cc3edf5873c51f5c
Hallejujah123 <57189226+Hallejujah123@users.noreply.github.com>
2023-05-19 14:14:37 +0300

    Update services.py

--- src/web/services.py
+++ src/web/services.py
@@ -1,7 +1,11 @@
 from pathlib import Path
 
 from src.typings import RawHTML, as_raw_html
+from src.config import Config
+
+config = Config()
 
 
-def load_html(path: Path) -> RawHTML:
+def load_html(file_name: str) -> RawHTML:
+    path = config.BASE_DIR / "src" / file_name
     return as_raw_html(path.read_text())
