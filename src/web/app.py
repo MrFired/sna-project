24398bb1b0f33a223956aeebe6a9d4c12c1d6107
Hallejujah123 <57189226+Hallejujah123@users.noreply.github.com>
2023-05-19 17:12:36 +0300

    Add web content (#2)
    * Add files via upload
    
    * Update style.css
    
    * Add files via upload
    
    * Update app.py
    
    * Update services.py
    
    * now it works
    
    * nice page
    
    ---------
    
    Co-authored-by: ADari-Ka <ADari.Ka@yandex.ru>

--- src/web/app.py
+++ src/web/app.py
@@ -1,22 +1,23 @@
 from fastapi import FastAPI
 from fastapi.responses import HTMLResponse
+from fastapi.staticfiles import StaticFiles
 
+from src import ROOT_DIR
 from src.config import Config
+from src.web.services import load_html
 
 app = FastAPI()
 config = Config()
 
 
+app.mount(
+    "/static",
+    StaticFiles(directory=(ROOT_DIR / config.STATIC_FILES)),
+    name="static",
+)
+
+
 @app.get("/")
 async def main_page():
-    html_content = """
-    <html>
-        <head>
-            <title>HoYoClub</title>
-        </head>
-        <body>
-            Just a text.
-        </body>
-    </html>
-    """
-    return HTMLResponse(content=html_content)
+    index_html = load_html("index.html", config)
+    return HTMLResponse(content=index_html)
