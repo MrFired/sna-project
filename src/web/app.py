97b0cf00bc9b396d3ac88cd79e77a0ca8c10542e
Hallejujah123 <57189226+Hallejujah123@users.noreply.github.com>
2023-05-19 13:59:39 +0300

    Update app.py

--- src/web/app.py
+++ src/web/app.py
@@ -1,11 +1,14 @@
 from fastapi import FastAPI
 from fastapi.responses import HTMLResponse
 
+
 from src.config import Config
 from src.services import load_html
 
+
 app = FastAPI()
 config = Config()
+
 
 @app.get("/")
 async def main_page():
